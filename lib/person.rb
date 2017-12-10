e06e66765e2e8dbf447d5e84bacfbc4659375022
Jawann <jawann@gmail.com>
2017-12-10 03:26:49 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :birthday, :color, :hair_color, :color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size
+  attr_accessor :name, :birthday, :color, :hair_color, :color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length
 
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
